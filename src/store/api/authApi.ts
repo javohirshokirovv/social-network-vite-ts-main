44ea4ee441e2b454df938027db51239334b5e1b6
Ваше Имя <.email@example.com>
2024-08-21 19:56:38 +0500

    init

--- src/store/api/authApi.ts
+++ src/store/api/authApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type { IGetUserResponse } from './types'
+import type { IGetUserResponse, ILoginUserPayload, ILoginUserResponse, IRegisterUserPayload, IRegisterUserResponse } from './types'
 
 export const authApi = createApi({
   reducerPath: 'authApi',
@@ -8,13 +8,20 @@ export const authApi = createApi({
     getUser: builder.query<IGetUserResponse, string>({
       query: (user_id) => `/user?user_id=${user_id}`,
     }),
-    loginUser: builder.mutation<any, any>({
+    loginUser: builder.mutation<ILoginUserResponse, ILoginUserPayload>({
         query: (loginData) => ({
             url: "/login",
             method: "POST",
             body: loginData,
         }),
     }),
+    registerUser: builder.mutation<IRegisterUserResponse, IRegisterUserPayload>({
+      query: (registerData) => ({
+          url: "/registration",
+          method: "POST",
+          body: registerData,
+      }),
+  }),
   }),
 });
 
--- src/store/api/types.ts
+++ src/store/api/types.ts
@@ -13,4 +13,25 @@ export interface IGetUserResponse {
     message: IUser
    
 }
-    
+
+export interface ILoginUserResponse{
+    status: number;
+    message: IUser;
+}
+export interface ILoginUserPayload{
+    email: string;
+    password: string;
+}
+
+export interface IRegisterUserResponse{
+
+}
+
+export interface IRegisterUserPayload{
+    name: string;
+    email: string;
+    phone_number: string;
+    password: string;
+    user_city: string;
+
+}
